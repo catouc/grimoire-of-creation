0a7ea881f982d5f002bcff2307ce5cb2048fde18
Philipp Boeschen <philipp.boeschen@tui.com>
2019-11-09 19:18:29 +0100

    Initial commit

---
+++ grimoire.py
@@ -0,0 +1,18 @@
+'''
+A powerful book containing all kinds of utility golems ready to server for
+their new master.
+'''
+
+from flask import Flask
+from flask_restful import Api
+
+from workshop import Golem 
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(Golem.Golems, '/golems')
+api.add_resource(Golem.Golem, '/golem/<golem_id>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ workshop/Golem.py
@@ -0,0 +1,49 @@
+'''
+Class that represents the golem API, contains methods to create update and
+destroy golems.
+'''
+
+from flask import request
+from flask_restful import Resource, reqparse
+
+golems = {}
+
+parser = reqparse.RequestParser()
+parser.add_argument('golem_name')
+parser.add_argument('golem_type')
+
+class Golem(Resource):
+    '''Golems are meant to be created and mindlessly work for it's creator.
+    '''
+    def get(self, golem_id):
+        return golems[golem_id]
+    
+    def delete(self, golem_id):
+        del golems[golem_id]
+        return '', 204
+
+    def put(self, golem_id):
+        json_data = request.get_json(force=True)
+        golem = {
+            'name': json_data['golem_name'],
+            'type': json_data['golem_type']
+        }
+        golems[golem_id] = golem
+        return golem, 201
+
+
+class Golems(Resource):
+    '''A master can never be sure what his creations are really up to
+    '''
+    def get(self):
+        return golems
+
+    def post(self):
+        json_data = request.get_json(force=True)
+        golem = {
+            'name': json_data['golem_name'],
+            'type': json_data['golem_type']
+        }
+        golem_id = len(golems)
+        golems[golem_id] = golem
+        return golem, 201
